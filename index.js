f3452225776b5277782b502dba7528607a3af2ca
Saikiran Biradar <saikiranbiradar76642@gmail.com>
2026-01-31 21:49:51 +0530

    Fixed nextjs issues

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = 3000;
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 8080;
 
-// Default route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('App is running on Elastic Beanstalk 🚀');
 });
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
